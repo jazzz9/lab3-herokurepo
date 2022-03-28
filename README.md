69f42a8d2c959972437497c2a217814a67c72bac
jazzz9 <jazzz9@my.yorku.ca>
2022-03-28 07:23:11 -0400

    final changes

--- README.md
+++
@@ -1,2 +0,0 @@
-# lab3-herokurepo
-This the my repo for Lab 03 Heroku Site
--- server.js
+++ server.js
@@ -1,29 +1,46 @@
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
+require("dotenv").config();
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
+const path = require("path");
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+const rooms = {};
 
-app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
-})
+io.on("connection", socket => {
+    socket.on("join room", roomID => {
+        if (rooms[roomID]) {
+            rooms[roomID].push(socket.id);
+        } else {
+            rooms[roomID] = [socket.id];
+        }
+        const otherUser = rooms[roomID].find(id => id !== socket.id);
+        if (otherUser) {
+            socket.emit("other user", otherUser);
+            socket.to(otherUser).emit("user joined", socket.id);
+        }
+    });
 
-app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
-})
+    socket.on("offer", payload => {
+        io.to(payload.target).emit("offer", payload);
+    });
 
-io.on('connection', socket => {
-  socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId)
+    socket.on("answer", payload => {
+        io.to(payload.target).emit("answer", payload);
+    });
 
-    socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId)
-    })
-  })
-})
-const PORT = process.env.PORT ||3000;
-server.listen(PORT)
+    socket.on("ice-candidate", incoming => {
+        io.to(incoming.target).emit("ice-candidate", incoming.candidate);
+    });
+});
+
+if(process.env.PROD) {
+    app.use(express.static(path.join(__dirname, './client/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, './client/build/index.html'));
+    });
+}
+const port = process.env.PORT || 8000;
+server.listen(port, () => console.log(`server is running on port ${port}`));
